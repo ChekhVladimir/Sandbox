1b7fc8c739d980930210d25ac225be16bc754b0e
ChekhVladimir <Chex.vowa@gmail.com>
2024-12-08 18:43:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
 import streamlit as st
 
 st.write('Hello World!')
+
+df = pd.read_csv("gym_members_exercise_tracking_synthetic_data.csv")
+st.write(df)
