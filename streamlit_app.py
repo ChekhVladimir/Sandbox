11e0d74326eabdae2e04d776d3f0fb64b5f12f34
ChekhVladimir <Chex.vowa@gmail.com>
2024-12-08 20:58:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import io
 
-st.code("
+st.code("""
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
-")
+""")
 
 st.set_page_config(page_title="Gym Member Analysis", layout="wide")
 
